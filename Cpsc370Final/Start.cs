4b08a29428d801c002cfac7fef1a87c4a1363122
kylesullyman <kylsullivan@chapman.edu>
2025-01-28 20:33:11 -0800

    base start.cs class

---
+++ Cpsc370Final/Start.cs
@@ -0,0 +1,13 @@
+namespace Cpsc370Final;
+
+public class Start
+{
+    private string[] args;
+
+    public bool isStartPhaseComplete = false;
+
+    public void beginStartPhase(string[] inputArgs)
+    {
+        args = inputArgs;
+    }
+}
