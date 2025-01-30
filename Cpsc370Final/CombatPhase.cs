599fa6a6796fda1cf89c02603330c86f18b9a8fb
kylesullyman <kylsullivan@chapman.edu>
2025-01-29 17:11:41 -0800

    added GetRandomEnemy() and PrintEnemyStats()

--- Cpsc370Final/CombatPhase.cs
+++ Cpsc370Final/CombatPhase.cs
@@ -2,8 +2,32 @@ namespace Cpsc370Final;
 
 public class CombatPhase
 {
+    public string chosenEnemy;
+
     public CombatPhase()
     {
-        
+        chosenEnemy = GetRandomEnemy().ToString();
+        Console.WriteLine("You are fighting a " + chosenEnemy);
+        Console.WriteLine("Enemy Stats:");
+        PrintEnemyStats();
+    }
+
+    private EnemyRoster GetRandomEnemy()
+    {
+        Array values = Enum.GetValues(typeof(EnemyRoster));
+        Random random = new Random();
+        return (EnemyRoster)values.GetValue(random.Next(values.Length));
+    }
+    
+    private void PrintEnemyStats()
+    {
+        Enemy enemy = new Enemy();
+        Console.WriteLine("Enemy Name: " + enemy.Name);
+        Console.WriteLine("Enemy Health: " + enemy.Health);
+        Console.WriteLine("Enemy Damage Dice: " + enemy.DamageDice);
+        Console.WriteLine("Enemy Dodge Chance: " + enemy.DodgeChance);
+        Console.WriteLine("Enemy Armor Class: " + enemy.ArmorClass);
+        Console.WriteLine("Enemy Special Damage: " + enemy.SpecialDamage);
+        Console.WriteLine("Enemy Special Cooldown: " + enemy.SpecialCooldown);
     }
 }
