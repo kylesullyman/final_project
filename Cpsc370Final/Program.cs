9c38b22eef21629726300f2f0de8c62989363cbb
Braeden <cootey@chapman.edu>
2025-01-29 16:19:30 -0800

    Added BeginStartPhase to main

--- Cpsc370Final/Program.cs
+++ Cpsc370Final/Program.cs
@@ -1,14 +1,18 @@
-﻿namespace Cpsc370Final;
-
+﻿using System.Diagnostics;
+namespace Cpsc370Final;
 class Program
 {
     static void Main(string[] args)
     {
-        if (args.Length < 1)
+        /*if (args.Length < 1)
             Console.WriteLine("Usage: Cpsc370Final <arguments>");
         
         // you can delete this if/when you like
         ShowArguments(args);
+        */
+        Start start = new Start();
+        start.BeginStartPhase();
+
     }
 
     // this is just an example of how to get the command
